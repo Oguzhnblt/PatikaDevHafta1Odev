dff9128d079bd85a387799919930fd32a4cc3a4a
Oğuzhan Bolat <oguzhanbolat0@gmail.com>
2023-02-06 18:45:51 +0300

    Entity düzenleme

--- PatikaDevHafta1Odev/Entities/Product.cs
+++ PatikaDevHafta1Odev/Entities/Product.cs
@@ -1,13 +1,18 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PatikaDevHafta1Odev.Entities
 {
 
     public class Product
     {
+        [Required]
+        [Column(Order = 1)]
         public int Id { get; set; }
+
         [Required]
         public string Name { get; set; }
+
         [Required]
         public decimal Price { get; set; }
         public string Description { get; set; }
